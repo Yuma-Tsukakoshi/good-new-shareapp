d7a60893be1687fc5d168e84b59eb58936806799
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-06 01:46:54 +0900

    add: init

--- src/index.php
+++ src/index.php
@@ -5,12 +5,12 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="./vendor/tailwind/tailwind.css" rel="stylesheet" />
-  <title>document</title>
+  <title>トップページ</title>
 </head>
 
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world
+  <h1 class="text-3xl font-bold underline text-center">
+    Good & New Sharing
   </h1>
 </body>
 
