cba98a5b69d4ae008d8e855f26a241e620cd2136
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-06 02:17:34 +0900

    add: database init

---
+++ src/admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href=""
+      rel="stylesheet"
+    />
+    <title>管理者画面</title>
+  </head>
+  <body>
+    <h1>good & new 管理者画面</h1>
+    
+  </body>
+</html>
