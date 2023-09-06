ef9414051e517ee076963206a1ccc7a1fdfb7fb9
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-06 10:42:02 +0900

    add:編集画面のフロント

--- src/admin/index.php
+++ src/admin/index.php
@@ -55,8 +55,7 @@ $posts = $pdo->query($sql)->fetchAll();
                       <td class="px-4 py-3">
                         <div class="flex items-center space-x-4 text-sm">
                           <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-blue-500 rounded-lg focus:outline-none focus:shadow-outline-gray" aria-label="Edit" data=<?= $post["id"] ?>>
-                            <!-- <a href="?id=<?= $post["id"] ?>">編集</a> -->
-                            編集
+                            <a href="http://localhost:8080/admin/posts/edit.php?id=<?= $post["id"] ?>">編集</a>
                           </button>
                           <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-gray-500 rounded-lg focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
                             <!-- onclick="hidePost(this)" 上記に追加-->
