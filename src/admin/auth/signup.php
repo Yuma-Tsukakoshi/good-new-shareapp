648d4d598f2cf9c377b7df907c5b864056f1f464
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-08 19:49:45 +0900

    add: 新規登録フロントエンド、バックエンド

--- src/admin/auth/signup.php
+++ src/admin/auth/signup.php
@@ -4,19 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>POSSEログイン画面</title>
+  <title>ログイン画面</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap"
     rel="stylesheet">
   <link rel="stylesheet" href="../../assets/styles/common.css">
-  <link rel="stylesheet" href="./admin.css">
+  <link rel="stylesheet" href="../admin.css">
 </head>
 
 <body>
-  <header class="p-header">
-    <div class="p-header__logo"><img src="../../assets/img/logo.svg" alt="POSSE"></div>
-  </header>
   <main>
   <h1>新規会員登録</h1>
     <form action="http://localhost:8080/admin/auth/register.php" method="post">
@@ -24,10 +21,19 @@
           <label>名前:<input type="text" name="name" required></label>
       </div>
       <div>
-          <label>メールアドレス：<input type="text" name="email" required></label>
+          <label>期生：<input type="text" name="kisei" required></label>
       </div>
       <div>
-          <label>パスワード：<input type="password" name="password" required></label>
+          <label>たてもく：<input type="text" name="tate" required></label>
+      </div>
+      <div>
+          <label>よこもく：<input type="text" name="yoko" required></label>
+      </div>
+      <div>
+          <label>誕生日：<input type="text" name="birthday" required></label>
+      </div>
+      <div>
+          <label>メールアドレス：<input type="text" name="mail" required></label>
       </div>
       <input type="submit" value="新規登録">
     </form>
