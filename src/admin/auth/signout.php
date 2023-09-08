7a4f20b56c0cd65405c2ffdb62faeb97a2582c87
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-08 19:53:48 +0900

    add: ログアウト遷移

--- src/admin/auth/signout.php
+++ src/admin/auth/signout.php
@@ -5,7 +5,6 @@ session_destroy();//セッションを破壊
 
 // $msg = "ログアウトしました";
 // echo $msg;
-header('Location: http://localhost:8080/admin/index.php');
+header('Location: http://localhost:8080/admin/auth/login_form.php');
 exit();
-
 ?>
--- src/admin/auth/signup.php
+++ src/admin/auth/signup.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ログイン画面</title>
+  <title>新規登録画面</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap"
