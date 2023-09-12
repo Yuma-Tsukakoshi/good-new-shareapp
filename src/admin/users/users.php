b764cb438c7a4976b9888b6a399702d41dcd0ebb
haruharumaru <paris3939rusk@icloud.com>
2023-09-12 20:49:31 +0900

    削除機能完成。しかし、ユーザー一覧画面では消したはずのユーザーのデータがリロードすると出てきてしまうなぜ...

--- src/admin/users/users.php
+++ src/admin/users/users.php
@@ -16,6 +16,8 @@ $users = $pdo->query($sql)->fetchAll();
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
   <link href="../../vendor/tailwind/tailwind.output.css" rel="stylesheet">
+  <!-- 9/12追加↓ -->
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
   <title>ユーザー一覧</title>
 </head>
 
